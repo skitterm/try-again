4e5cc0b60f5bda3a119d2f219e44972f1452fdca
Steven Kitterman <steven.kitterman@gmail.com>
2025-05-13 22:17:45 -0700

    asdf

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,5 +2,5 @@
 import { test, expect } from "@chromatic-com/playwright";
 
 test("has title", async ({ page }) => {
-  await page.goto("http://localhost:5173/");
+  await page.goto("http://localhost:5173/"); //jah
 });
